a52381a44afd315c17e3ba13fbdd9185608263e5
MARK VEERASINGAM - STUDENT <g00403931@atu.ie>
2023-11-08 14:30:05 +0000

    added OrderController

--- src/main/java/ie/atu/labexam/OrderService.java
+++ src/main/java/ie/atu/labexam/OrderService.java
@@ -1,2 +1,18 @@
-package ie.atu.labexam;public class OrderService {
+package ie.atu.labexam;
+
+import java.util.ArrayList;
+
+public class OrderService {
+    private Product product;
+
+    ArrayList<Product> products = new ArrayList<>();
+
+    public void createOrder(Product product){
+        products.add(product);
+        System.out.println("Order Added " + product.getProductId());
+    }
+
+    public int getOrderById() {
+        return product.getOrderId();
+    }
 }
