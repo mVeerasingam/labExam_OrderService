3e66b7fe708e3fdfbe1c9b59cda018e9f1893905
MARK VEERASINGAM - STUDENT <g00403931@atu.ie>
2023-11-08 14:24:22 +0000

    added order service class

---
+++ src/main/java/ie/atu/labexam/OrderService.java
@@ -0,0 +1,2 @@
+package ie.atu.labexam;public class OrderService {
+}
--- src/main/java/ie/atu/labexam/Product.java
+++ src/main/java/ie/atu/labexam/Product.java
@@ -1,2 +1,15 @@
-package ie.atu.labexam;public class Product {
+package ie.atu.labexam;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private int orderId;
+    private int productId;
+    private int quantity;
+    private  int customerId;
 }
